9177c300b995c93632c6ceb17acb9530809a66f1
Abdurozzaq Nurul Hadi <Abdurozzaq@outlook.com>
2021-05-03 17:19:49 +0700

    Selesai No 3

--- abdurozzaqnurulhadi_soal_3.php
+++ abdurozzaqnurulhadi_soal_3.php
@@ -5,4 +5,31 @@ mengeluarkan Rp 10.000 biaya pengeluaran untuk toko kelontong ke 1. dan biaya
 pengeluaran untuk toko kelontong ke 68 adalah sebesar Rp. 30.100 . maka
 berapakah biaya pengeluaran untuk toko kelontong ke 81 ?</pre>";
 
-echo "<h3>Penyelesaian</h3><br>";
+echo "<h3>Penyelesaian</h3>";
+
+echo "Menggunakan RUMUS Matematika Barisan Aritmatika<br>";
+
+// Diketahui
+$banyakCabang = 100;
+$biayaToko1 = 10000;
+$biayaToko68 = 30100;
+
+echo "Diketahui: <br>";
+echo "Banyak Cabang = " . $banyakCabang . "<br>";
+echo "Biaya Toko 1 = " . $biayaToko1 . "<br>";
+echo "Biaya Toko 68 = " . $biayaToko68 . "<br><br>";
+
+echo "Penyelesaian: <br>";
+echo "Mencari Selisih Tiap Toko: <br>";
+$selisih68Dan1 = $biayaToko68 - $biayaToko1;
+$selisihUrutanToko = 68 - 1;
+$bedaTiapToko = $selisih68Dan1 / $selisihUrutanToko;
+echo "Selisih/Beda Tiap Toko Adalah: " . $bedaTiapToko . "<br><br>";
+
+echo "Mencari Biaya Toko Ke 81: <br>";
+echo "RUMUS: 81 = 10000 + (81-1) * 300<br>";
+$nmin1 = 81 - 1;
+$biayaToko81 = $biayaToko1 + $nmin1 * $bedaTiapToko;
+echo "<strong>JADI: Biaya Pengeluaran Untuk Toko Kelontong ke 81 Adalah " . "$biayaToko81 </strong>";
+
+
