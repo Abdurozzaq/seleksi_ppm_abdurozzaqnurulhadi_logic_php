ed42235ea551fd1a32251a50e783281ac878690d
Abdurozzaq Nurul Hadi <Abdurozzaq@outlook.com>
2021-05-03 17:33:52 +0700

    Perbaikan harus pakai looping ternyata

--- abdurozzaqnurulhadi_soal_3.php
+++ abdurozzaqnurulhadi_soal_3.php
@@ -7,8 +7,6 @@ berapakah biaya pengeluaran untuk toko kelontong ke 81 ?</pre>";
 
 echo "<h3>Penyelesaian</h3>";
 
-echo "Menggunakan RUMUS Matematika Barisan Aritmatika<br>";
-
 // Diketahui
 $banyakCabang = 100;
 $biayaToko1 = 10000;
@@ -26,10 +24,21 @@ $selisihUrutanToko = 68 - 1;
 $bedaTiapToko = $selisih68Dan1 / $selisihUrutanToko;
 echo "Selisih/Beda Tiap Toko Adalah: " . $bedaTiapToko . "<br><br>";
 
-echo "Mencari Biaya Toko Ke 81: <br>";
-echo "RUMUS: 81 = 10000 + (81-1) * 300<br>";
-$nmin1 = 81 - 1;
-$biayaToko81 = $biayaToko1 + $nmin1 * $bedaTiapToko;
+echo "Looping untuk mencari biaya toko ke 81 dengan acuan Selisih/Beda Tiap Toko<br>";
+
+$biayaToko81 = $biayaToko68;
+for ($i = 69; $i <= 81; $i++) {
+    $biayaToko81 = $biayaToko81 + 300;
+}
+echo "Selesai Looping... <br>";
 echo "<strong>JADI: Biaya Pengeluaran Untuk Toko Kelontong ke 81 Adalah " . "$biayaToko81 </strong>";
+
+
+// echo "Menggunakan RUMUS Matematika Barisan Aritmatika<br>";
+// echo "Mencari Biaya Toko Ke 81: <br>";
+// echo "RUMUS: 81 = 10000 + (81-1) * 300<br>";
+// $nmin1 = 81 - 1;
+// $biayaToko81 = $biayaToko1 + $nmin1 * $bedaTiapToko;
+// echo "<strong>JADI: Biaya Pengeluaran Untuk Toko Kelontong ke 81 Adalah " . "$biayaToko81 </strong>";
 
 
